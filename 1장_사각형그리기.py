c200fa67ce18f1391fc7f20a3f5b24ec5388ffda
milktoko <milktoko@milktokoui-MacBookAir.local>
2025-09-08 10:53:05 +0900

    first commit

---
+++ 1장_사각형그리기.py
@@ -0,0 +1,14 @@
+import turtle
+t = turtle.Turtle()
+t.shape("classic")
+t.width(10)
+
+t.forward(200)
+t.left(90)
+t.fd(200)
+t.left(90)
+t.fd(200)
+t.left(90)
+t.fd(200)
+
+turtle.done()
---
+++ 1장_삼각형그리기.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Turtle()
+t.shape("classic")
+t.width(10)
+
+t.fd(200)
+t.left(120)
+t.fd(200)
+t.left(120)
+t.fd(200)
