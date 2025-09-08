811e876fbe0d7f6121baa0b949c7b89752400a21
toywoo <39830308+toywoo@users.noreply.github.com>
2025-09-08 12:20:41 +0900

    2025-09-08-12-20

---
+++ 2장_원의 둘레.py
@@ -0,0 +1,9 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+radius = 100
+t.circle(radius) # 반지름이 100인 원
+t.fd(30)
+t.circle(radius) # 반지름이 100인 원
+t.fd(30)
+t.circle(radius) # 반지름이 100인 원
---
+++ 2장_정수확인.py
@@ -0,0 +1,9 @@
+a = int(input("정수1 : "))
+b = int(input("정수2 : "))
+c = int(input("정수3 : "))
+
+sum = a + b + c
+avg = sum / 3
+
+print("정수1 :", a, "정수2 :", b, "정수3 :", c)
+print("합 :", sum, "평균 :", avg)
